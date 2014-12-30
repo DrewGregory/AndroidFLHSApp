86be36b6f86a2d34e90024d874b2296db4f0253c
Drew Gregory <djgregny@gmail.com>
2014-12-29 22:00:35 -0500

    Added AboutActivtiy. Changed FLHS Activity so that it can redirect to AboutActivity.

---
+++ app/src/main/java/com/flhs/AboutActivity.java
@@ -0,0 +1,20 @@
+/* Written by Drew Gregory: 2/20/2014
+*/
+package com.flhs;
+
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AboutActivity extends FLHSActivity{
+	Button b1;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		SetupNavDrawer();
+		
+	}
+}
