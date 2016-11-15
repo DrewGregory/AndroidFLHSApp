ab4121633b548be8bef1ac63c7c5fe23924b9033
Drew <djgregny@gmail.com>
2016-11-14 21:44:09 -0500

    Finally finished. I look forward to discussing this with you tomorrow @Theo :-).

--- app/src/main/java/com/flhs/FLHSApplication.java
+++ app/src/main/java/com/flhs/FLHSApplication.java
@@ -19,6 +19,7 @@ import android.content.SharedPreferences;
 import android.util.Log;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class FLHSApplication extends Application {
@@ -37,9 +38,12 @@ public class FLHSApplication extends Application {
                 .build()
         );
 
-        /* This code generated my 1 "Config" object. ParseConfig was discontinued in Parse Server. :-(
-        ParseObject gameScore = new ParseObject("Config");
-        gameScore.put("WhatDay", "[]");
+       /*  //This code generated my 1 "Config" object. ParseConfig was discontinued in Parse Server. :-(
+        ParseObject gameScore = new ParseObject("ConfigObject");
+        String[] days = {
+                "10/26:3","10/27:Special 5","10/31:4","11/01:5","11/02:A","11/03:B","11/04:C","11/07:D","11/08:E","11/09:1","11/10:Special 2","11/14:3","11/15:4","11/16:Collab 5","11/17:A","11/18:B","11/21:C","11/22:D","11/23:Special E","11/28:1","11/29:2","11/30:3","12/01:4","12/02:5","12/05:Special A","12/06:B","12/07:C","12/08:D","12/09:E","12/12:1","12/13:2","12/14:3","12/15:4","12/16:Collab 5","12/19:A","12/20:B","12/21:C","12:22:D","12/23:Special E"
+        };
+        gameScore.put("WhatDay", Arrays.asList(days));
         gameScore.put("LunchMenuURL", "http://www.bcsdny.org/files/filesystem/Nov%20HS%20MS%20Lunch.pdf");
         gameScore.put("MAGIC", true);
         gameScore.saveInBackground(new SaveCallback() {
@@ -50,19 +54,8 @@ public class FLHSApplication extends Application {
                     Log.i("Parse", "Save Failed");
                 }
             }
-        });  */
+        });*/
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Config");
-        query.getInBackground("ErmEX6MwFz", new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
-                if (e == null) {
-                    // object will be your game score
-                    Log.i("IT WORKED!", object.getString("WhatDay"));
-                } else {
-                    // something went wrong
-                }
-            }
-        });
 
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
